80e09ec7563178959e64b2147ff6fed467b780d5
Evans Musomi <evans.musomi@gmail.com>
2017-05-13 06:15:04 +0300

    Adds about page template and route

--- routes.py
+++ routes.py
@@ -11,5 +11,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    """ Renders about page """
+    return render_template("about.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
