8e713265575010539194f9e76b0abe10ece1ac80
Evans Musomi <evans.musomi@gmail.com>
2017-05-13 06:04:23 +0300

    Maps index route to home page

---
+++ routes.py
@@ -0,0 +1,15 @@
+""" Maps browser requests to Python functions """
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """ Renders home page """
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
