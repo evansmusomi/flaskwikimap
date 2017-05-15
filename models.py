46bab8f28a78251ac62a86ae736672f0bfb482d4
Evans Musomi <evans.musomi@gmail.com>
2017-05-15 05:51:10 +0300

    Connects flask to database using sqlalchemy and adds user model

---
+++ models.py
@@ -0,0 +1,31 @@
+""" Defines data structures to use to read and write to tables """
+
+from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    """ Defines data structure for reading and writing to users table """
+
+    __tablename__ = 'users'
+    uid = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    email = db.Column(db.String(120), unique=True)
+    pwdhash = db.Column(db.String(54))
+
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.email = email.lower()
+        self.set_password(password)
+
+    def set_password(self, password):
+        """ Generates password hash from password """
+        self.pwdhash = generate_password_hash(password)
+
+    def check_password(self, password):
+        """ Compares password hash with password """
+        return check_password_hash(self.pwdhash, password)
--- routes.py
+++ routes.py
@@ -1,8 +1,13 @@
 """ Maps browser requests to Python functions """
 
+from models import db
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+# Connects Flask to database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/flaskwikimap'
+db.init_app(app)
 
 
 @app.route("/")
