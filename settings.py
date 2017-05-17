712122d95ab412ddbef43feb75ebc7148cd1b960
Evans Musomi <evans.musomi@gmail.com>
2017-05-17 06:38:11 +0300

    Adds ability to read env settings and updates README for final deploy.

---
+++ settings.py
@@ -0,0 +1,15 @@
+""" Loads environment configuration """
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+
+class Config(object):
+    """ Defines config variables """
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get(
+        "SQLALCHEMY_TRACK_MODIFICATIONS")
